8743f1e0d8aba5cde339db8d47562073b114ef43
Sahil <62090073+Sahil-31@users.noreply.github.com>
2020-03-21 16:53:45 -0400

    Add files via upload
    this is bash script

---
+++ bash.sh
@@ -0,0 +1,14 @@
+
+echo "enter your name"
+read  a
+# case "$a" in
+echo "enter your age"
+read b 
+#echo "$a -eq sahil"
+if [ $b -gt 18 ];then
+	echo "$b is eligable"
+else
+	echo "uneligable"
+fi 
+
+
